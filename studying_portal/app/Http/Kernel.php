96ec8da64e949331c8f05ad8a3ba485d9a44a454
Youssef Shaker <youssef.a.shaker@gmail.com>
2024-11-02 09:45:22 +0200

    create laravel project

---
+++ studying_portal/app/Http/Kernel.php
@@ -0,0 +1,16 @@
+<?php
+// app/Http/Kernel.php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    protected $middlewareGroups = [
+        'api' => [
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+}
